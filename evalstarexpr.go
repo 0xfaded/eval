cbd97b4fa9b01a7eb2b7191a1c213273263d6b61
Carl Chatfield <carlchatfield@gmail.com>
2014-01-21 18:53:33 +0900

    Check star expressions test gen.

--- evalstarexpr.go
+++ evalstarexpr.go
@@ -5,7 +5,7 @@ import (
 )
 
 func evalStarExpr(ctx *Ctx, starExpr *StarExpr, env *Env) (reflect.Value, error) {
-	if v, _, err := EvalExpr(ctx, starExpr, env); err != nil {
+	if v, _, err := EvalExpr(ctx, starExpr.X.(Expr), env); err != nil {
 		return reflect.Value{}, err
 	} else {
 		return (*v)[0].Elem(), nil
