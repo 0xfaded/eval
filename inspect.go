8c6df36e148210aeec96f3e5bab3d77909b8aca0
Carl Chatfield <carlchatfield@gmail.com>
2014-02-12 23:47:56 +0900

    Fixed inspect display bugs.

--- inspect.go
+++ inspect.go
@@ -26,13 +26,10 @@ func Inspect(val reflect.Value) string {
 	switch val.Kind() {
 	case reflect.String:
 		return strconv.QuoteToASCII(val.String())
-	case reflect.Bool, reflect.Int,	reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr, reflect.Float32, reflect.Float64, reflect.Complex64, reflect.Complex128:
-		if val.CanInterface() {
-			fmt.Sprintf("%v", val.Interface())
-		} else {
-			fmt.Sprintf("<%v>", val.Type())
-		}
 	case reflect.Slice, reflect.Array:
+		if val.Len() == 0 {
+			return "[]"
+		}
 		sep := "{"
 		str := ""
 		for i:=0; i < val.Len(); i++ {
@@ -44,31 +41,42 @@ func Inspect(val reflect.Value) string {
 		return str
 
 	case reflect.Struct:
-		str := "{"
+		if val.Type() == untypedNilType {
+			return "nil"
+		}
 		n  := val.NumField()
+		if n == 0 {
+			return "{}"
+		}
+		str := ""
+		sep := "{"
 		for i := 0; i < n; i++ {
 			if i > 0 { str += " " }
 			name  := val.Type().Field(i).Name
 			field := val.Field(i)
-			str += fmt.Sprintf("%s: %s,", name, Inspect(field))
+			str += fmt.Sprintf("%s%s: %s", sep, name, Inspect(field))
+			sep = ","
 		}
 		str += "}"
 		return str
 
 	case reflect.Ptr:
-		if val.IsNil() {
+		// Internal const numbers
+		i := val.Interface()
+		if cn, ok := i.(*ConstNumber); ok {
+			return fmt.Sprint(cn)
+		} else if val.IsNil() {
 			return "nil"
 		} else {
 			return "&" + Inspect(reflect.Indirect(val))
 		}
-	}
-
-
-	// FIXME: add more Kinds as folks are annoyed with the output of
-	// the below:
-	if val.CanInterface() {
-		return fmt.Sprintf("%v", val.Interface())
-	} else {
-		return fmt.Sprintf("<%v>", val.Type())
+	default:
+		// FIXME: add more Kinds as folks are annoyed with the output of
+		// the below:
+		if val.CanInterface() {
+			return fmt.Sprintf("%v", val.Interface())
+		} else {
+			return fmt.Sprintf("<%v>", val.Type())
+		}
 	}
 }
